c1f1576b65fa573e2c11629ff292c1b1a30e951e
Diego Lume <diego.lume.a@gmail.com>
2018-05-10 15:44:30 -0500

    Elm adde

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,11 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+
+const elmDiv = document.getElementById("elm-main");
+Elm.Main.embed(elmDiv);
+
+
 // Import local files
 //
 // Local files can be imported directly using relative
